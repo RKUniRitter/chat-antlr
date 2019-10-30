66076f5e8e8852f0f391f987949011c2cf134ebf
Roger Krolow <roger.krolow@gmail.com>
2019-10-29 21:27:04 -0300

    Update ChatMain.java

--- src/ChatMain.java
+++ src/ChatMain.java
@@ -7,7 +7,7 @@ public class ChatMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//CharStream input = CharStreams.fromString("hello there");
-		CharStream input = CharStreams.fromString("john SAYS: hello @michael this will not work");
+		CharStream input = CharStreams.fromString("john SAYS: hello @michael this will not work\n");
 		ChatLexer lexer = new ChatLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		ChatParser parser = new ChatParser(tokens);
